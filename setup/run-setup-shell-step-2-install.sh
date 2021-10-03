ce7419e91f9629a3d7792b55f4fb2763240c9c26
Andrew Forrester <awforrester@gmail.com>
2021-10-02 17:47:40 -0700

    Add gh (GitHub-CLI) to installation processes.

--- setup/run-setup-shell-step-2-install.sh
+++ setup/run-setup-shell-step-2-install.sh
@@ -38,6 +38,14 @@ if [[ `uname -s` = "Linux" ]] ; then
     if [[ -z `which thefuck` ]] ; then
         sudo pip3 install thefuck
     fi
+    # Check/Install gh (the GitHub-CLI)
+    if [[ -z `which gh` ]] ; then
+        # See https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+        sudo apt update
+        sudo apt install gh
+    fi
 
     # See how update goes now:
     echo "Running \"aptup\" again after any installs (or none)."
