17c3e29bdb67be0ea3dd5c0da3b2020734d9e90a
Andrew Forrester <andrew.forrester@pacden.com>
2024-01-29 11:21:00 -0800

    Use proper PowerShell command.

--- config/win/wsl2-fix/wsl2net-fix.ps1
+++ config/win/wsl2-fix/wsl2net-fix.ps1
@@ -45,7 +45,7 @@ Write-Output "Initial routing table:"
 bash -c "sudo ip route show"
 $guest_ip = bash -c "/sbin/ifconfig eth0 | egrep -o 'inet [0-9\.]+' | cut -d ' ' -f2"
 Write-Output "Guest IP IS: $guest_ip"
-$gateway_ips = Get-NetIPAddress -InterfaceAlias "vEthernet (WSL)" | select IPAddress
+$gateway_ips = Get-NetIPAddress -InterfaceAlias "vEthernet (WSL)" | Select-Object IPAddress
 $gateway_ip = $gateway_ips[1].IPAddress
 Write-Output "Gateway (local WSL adapter) IP is: $gateway_ip"
 bash -c "sudo ifconfig eth0 netmask 255.255.240.0"
